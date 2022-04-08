fade8c2996430022e5fa0813e93db88978c42bc2
Rowings <zakariaazarkan@gmail.com>
2022-04-08 10:32:35 +0200

    2.6 fini

---
+++ 2.6.js
@@ -0,0 +1,32 @@
+// var x = Number(prompt("Put a number between 1&7"))
+// let one = "Monday"
+// let two = "Tuesday"
+// let three = "Wednesday"
+// let four = "Thursday"
+// let five = "Friday"
+// let six = "Saturday"
+// let seven = "Sunday"
+
+
+let day = prompt("Put a number between 1&7");
+
+if (day == 1) {
+    alert("Lundi")
+} else if (day == 2) {
+    alert("Mardi")
+} else if (day == 3) {
+    alert("Mercredi")
+} else if (day == 4) {
+    alert("Jeudi")
+} else if (day == 5) {
+    alert("Vendredi")
+} else if (day == 6) {
+    alert("Samedi")
+} else if (day == 7) {
+    alert("Dimanche")
+}
+
+
+
+
+
