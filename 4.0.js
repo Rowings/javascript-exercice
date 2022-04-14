7cdd335f07b7ed6e065aa80490b5ae459122ac03
Rowings <zakariaazarkan@gmail.com>
2022-04-14 10:37:14 +0200

    4.1 fini

---
+++ 4.0.js
@@ -0,0 +1,12 @@
+// function calcSurface() {
+
+// }
+
+        let length = prompt("Entre la longueur");
+        let width = prompt("Entre la largeur.");
+
+        function area(length, width) {
+            return length * width;
+        }
+        
+        console.log('La surface de ton rectanle est : ' + area(length, width));  
