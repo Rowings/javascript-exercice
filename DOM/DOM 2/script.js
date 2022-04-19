21e4022f4d14bcf455e24991a2824cdcbe6fb571
Rowings <zakariaazarkan@gmail.com>
2022-04-19 12:21:16 +0200

    dom selector point 2 fini

--- DOM/DOM 2/script.js
+++ DOM/DOM 2/script.js
@@ -1,7 +1,10 @@
 /*
 */
 
+
+
  let allImportant = document.querySelectorAll(".important")
+ let allImg = document.querySelectorAll("img")
 
  for (let index = 0; index < allImportant.length; index++) {
      const element = allImportant[index];
@@ -9,3 +12,21 @@
      console.log(allImportant)
      
     }
+
+    for (let index = 0; index < allImg.length; index++) {
+        const element = allImg[index];
+        
+        if(element.className !== "important") {
+            element.style.display = "none";
+        }
+    }
+//     for (let element of document.getElementsByClassName("important")){
+//         element.style.display="inital";
+
+//         if(element.className !== "important") {
+//             element.style.display = "none";
+//     }
+
+        
+//   }
+
